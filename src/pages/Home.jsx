0396fe555c04af38d250cc687ee9765aec4cc0d1
Valik <valik3201@gmail.com>
2024-02-06 12:55:30 +0100

    feat(api): add function to fetch trending data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,44 @@
+import { useState, useEffect } from 'react';
+import { fetchTrendingData } from 'services/api';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const trendingData = await fetchTrendingData();
+        setMovies(trendingData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending Movies and TV Shows</h1>
+      <div>
+        {movies.map(movie => (
+          <div key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt={movie.title || movie.name}
+            />
+            <p>
+              {movie.media_type === 'movie' ? 'Title' : 'Name'}:{' '}
+              {movie.title || movie.name}
+            </p>
+            <p>Media Type: {movie.media_type}</p>
+            <p>
+              {movie.media_type === 'movie' ? 'Release Date' : 'First Air Date'}
+              : {movie.release_date || movie.first_air_date}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
