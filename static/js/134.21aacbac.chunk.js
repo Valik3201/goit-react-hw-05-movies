9f774aa61fbd53e36231e0a772fa9f360b72903f
Valik3201 <valik3201@gmail.com>
2024-02-06 12:19:47 +0000

    Deploying to gh-pages from @ Valik3201/goit-react-hw-05-movies@85819fbf94ff76ac564559f42babfd51418aa86f 🚀

---
+++ static/js/134.21aacbac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[134],{134:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Movies"})}}}]);
+//# sourceMappingURL=134.21aacbac.chunk.js.map
---
+++ static/js/805.5bc38000.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,t,i){i.r(t);var n=i(184);t.default=function(){return(0,n.jsx)("div",{children:"Not Found"})}}}]);
+//# sourceMappingURL=805.5bc38000.chunk.js.map
