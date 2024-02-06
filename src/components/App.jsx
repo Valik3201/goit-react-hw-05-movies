b8a2791e843c0c9514f87366782423ef119b5083
Valik <valik3201@gmail.com>
2024-02-06 11:15:10 +0100

    feat: implement initial routing structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+import { NotFound } from 'pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/SharedLayout.jsx
@@ -0,0 +1,16 @@
+// Imports
+import { Outlet, Link } from 'react-router-dom';
+
+export const SharedLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
